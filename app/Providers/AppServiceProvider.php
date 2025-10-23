decdefabed0131560aa94281db22d7a055259623
Anas <anas.bettouzia@esprit.tn>
2025-10-23 21:52:11 +0100

    Resolve conflict Gestion Forum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,12 +12,13 @@ use Illuminate\Support\ServiceProvider;
 use App\Models\User;
 use Illuminate\Notifications\DatabaseNotification;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(\Laravel\Pail\PailServiceProvider::class)) {
-            $this->app->register(\Laravel\Pail\PailServiceProvider::class);
+        if ($this->app->environment('local') && class_exists('\Laravel\Pail\PailServiceProvider')) {
+            $this->app->register('\Laravel\Pail\PailServiceProvider');
         }
     }
 
